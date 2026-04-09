c610a100de093d6c279e652797b5dd0970e6e2c4
Vaibhav <vaibhav708378@gmail.com>
2026-04-09 20:02:14 +0530

    Array

--- Arrays/Arrays1.java
+++ Arrays/Arrays1.java
@@ -1,3 +1,4 @@
+//Defining a array 1.
 public class Arrays1 {
     public static void main(String[] args) {
         //int[] marks = new int[3];
---
+++ Arrays/Arrays2.java
@@ -0,0 +1,11 @@
+//Defining a array 2.
+public class Arrays2 {
+    public static void main(String[] args) {
+        // int[] marks = {97, 95, 92};
+        int marks[] = {97, 95, 92};
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
