2b84ed599dfd36874a70344f0598820cbe0affb5
Vaibhav <vaibhav708378@gmail.com>
2026-04-11 19:40:09 +0530

    Strings

---
+++ Strings/Declaration.java
@@ -0,0 +1,10 @@
+public class Declaration {
+    public static void main(String[] args) {
+        //String Declaration
+        String name = "Vaibhav";
+        String fullName = "Vaibhav Patil";
+
+        System.out.println(name);
+        System.out.println(fullName);
+    }
+}
