5d32e212228f36cc7860ec6b7839c0c8da3af905
Vaibhav <vaibhav708378@gmail.com>
2026-04-14 20:57:48 +0530

    Question

---
+++ Strings/String Builder/Ques.java
@@ -0,0 +1,12 @@
+public class Ques {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+
+        for(int i = 0; i < sb.length()/2; i++) {
+            int front = i;
+            int back = sb.length() - 1 - i;
+
+            
+        }
+    }
+}
