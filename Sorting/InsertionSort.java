33c20d7a4c648c158ca7bbabd83aaeb07d579bb1
Vaibhav <vaibhav708378@gmail.com>
2026-05-08 19:30:17 +0530

    Insertion Sort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+public class InsertionSort {
+
+    public static void printArray(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 2};
+
+        //Insertion Sort
+        for(int i = 0; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            //Placement
+            arr[j + 1] = current;
+        }
+
+        printArray(arr);
+    }
+}
