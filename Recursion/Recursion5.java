3e00e07a48d6831a2f8cd527cc7873b83ca725fa
Vaibhav <vaibhav708378@gmail.com>
2026-05-10 20:22:59 +0530

    Recursion 5

---
+++ Recursion/Recursion5.java
@@ -0,0 +1,26 @@
+// Q5. Print the Fibonacci series till nth term.
+
+public class Recursion5 {
+
+    public static void fibonacci(int a, int b, int n) {
+
+        if (n == 0) {
+            return;
+        }
+
+        int c = a + b;
+        System.out.println(c);
+        fibonacci(b, c, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int n = 7;
+        fibonacci(a, b, n - 2);
+    }
+}
