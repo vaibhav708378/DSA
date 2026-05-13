4b2cd37a68c5f7622ad78495bf478a06020f8206
Vaibhav <vaibhav708378@gmail.com>
2026-05-13 19:57:28 +0530

    Recursion 2.6

---
+++ Recursion/Recursion6.java
@@ -0,0 +1,32 @@
+// Q6, Remov duplicates in a String
+
+public class Recursion6 {
+
+    public static boolean map[] = new boolean[26];
+
+    public static void removeDuplicates(String str, int idx, String newString ) {
+
+        if (idx == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+
+        if (map[currChar - 'a']) {
+            removeDuplicates(str, idx + 1, newString);
+        } else {
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx + 1, newString);
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        String str = "abbccda";
+
+        removeDuplicates(str, 0, "");
+        
+    }
+}
