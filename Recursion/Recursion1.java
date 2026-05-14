b5ab463dc159f06a25897e603e0e1ab2d44130c2
Vaibhav <vaibhav708378@gmail.com>
2026-05-14 19:17:52 +0530

    Recursion 3.1

---
+++ Recursion/Recursion1.java
@@ -0,0 +1,24 @@
+// Q1. Print all permutations of a string.
+
+public class Recursion1 {
+
+    public static void printPerm(String str, String permutation) {
+
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for(int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPerm(newStr, permutation + currChar);
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
